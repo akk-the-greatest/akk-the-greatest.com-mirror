77156e9dd961e85bd56b6e4a98cd3e97b70a0d6f
akk <akk.the.greatest1@gmail.com>
2025-04-18 12:25:45 +0200

    another fix

--- fix_paths.sh
+++ fix_paths.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# Fixes all HTML files for GitHub Pages subdirectory hosting
 
-find . -name "*.html" -type f | while read file; do
-  sed -i 's|href="/|href="|g' "$file"
-  sed -i 's|src="/|src="|g' "$file"
+GHPATH="/akk-the-greatest.com-mirror"
+
+find . -type f -name "*.html" | while read file; do
+  sed -i "s|href=\"/|href=\"$GHPATH/|g" "$file"
+  sed -i "s|src=\"/|src=\"$GHPATH/|g" "$file"
+  sed -i "s|url(/|url($GHPATH/|g" "$file" # for CSS background-image URLs
 done
